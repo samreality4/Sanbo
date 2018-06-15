5f78b3befcec75cdc28ae858dd63e4bd802b45a0
sam <swannsee20@gmail.com>
2018-06-15 13:47:21 -0700

    feat: Adding new location search

--- app/src/main/java/com/sxg/sam/sanbo/MapsActivity.kt
+++ app/src/main/java/com/sxg/sam/sanbo/MapsActivity.kt
@@ -1,7 +1,10 @@
 package com.sxg.sam.sanbo
 
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.places.Places
 import com.google.android.gms.location.places.Places.getGeoDataClient
@@ -13,10 +16,19 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import jdk.nashorn.internal.runtime.ECMAException.getException
+import android.support.test.orchestrator.junit.BundleJUnitUtils.getResult
+import org.junit.experimental.results.ResultMatchers.isSuccessful
+import android.support.annotation.NonNull
+import android.support.v4.app.FragmentActivity
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+abstract class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+   private var mLocationPermissionGranted : Boolean = false
 
     private lateinit var mMap: GoogleMap
 
@@ -29,15 +41,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
 
+        // Construct a GeoDataClient.
+        getGeoDataClient(this);
 
-            // Construct a GeoDataClient.
-            mGeoDataClient = getGeoDataClient(this);
+        // Construct a PlaceDetectionClient.
+        getPlaceDetectionClient(this);
 
-            // Construct a PlaceDetectionClient.
-            mPlaceDetectionClient = getPlaceDetectionClient(this);
-
-            // Construct a FusedLocationProviderClient.
-            mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        // Construct a FusedLocationProviderClient.
+        LocationServices.getFusedLocationProviderClient(this);
 
     }
 
@@ -57,11 +68,45 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
     }
 
 
+
+    private void getDeviceLocation(){
+        /*
+     * Get the best and most recent location of the device, which may be null in rare
+     * cases when a location is not available.
+     */
+        try {
+            if (mLocationPermissionGranted) {
+                val locationResult = mFusedLocationProviderClient.getLastLocation()
+                locationResult.addOnCompleteListener(this, object : OnCompleteListener {
+                    fun onComplete(task: Task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mLastKnownLocation = task.getResult()
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), DEFAULT_ZOOM))
+                        } else {
+                            Log.d(FragmentActivity.TAG, "Current location is null. Using defaults.")
+                            Log.e(FragmentActivity.TAG, "Exception: %s", task.getException())
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM))
+                            mMap.uiSettings.isMyLocationButtonEnabled = false
+                        }
+                    }
+                })
+            }
+        } catch (e: SecurityException) {
+
+        }
+
+
+    }
+
 }
-}
+
 
 
 //find restaurants that are nearby you right now and at your current location.
