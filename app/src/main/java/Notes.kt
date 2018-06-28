ff337942ef25300ccc3cb16ba9dd46a593ac8e1f
sam <swannsee20@gmail.com>
2018-06-27 21:20:00 -0700

    feat: add Location

--- app/src/main/java/Notes.kt
+++ app/src/main/java/Notes.kt
@@ -2,3 +2,9 @@
 //Had to initial Location with an empty string.
 //object : Callback() = new Callback()
 //private  var mMap: GoogleMap? = null to initialize in kotlin
+//use Terminal rm --cached gradle.properties to exclude it after you upload it
+//put gradle.properties in .gitignore to ignore it.
+//reference api key in gradle.properties
+// put the following build gradle module to access api from anywhere
+/*resValue 'string', "api_key", MyAwesomeApp_ApiKey
+buildConfigField 'String', "ApiKey", MyAwesomeApp_ApiKey*/
